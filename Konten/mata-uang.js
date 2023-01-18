e23f8585c80be45d7a5963404b2163efba1bd95c
Raditya-P-H <radityahidayat917@gmail.com>
2023-01-18 12:34:45 +0700

    membuat konversi mata uang

--- Konten/mata-uang.js
+++ Konten/mata-uang.js
@@ -1,1 +1,4 @@
-let
+const select = document.querySelectorAll('.currency');
+const btn = document.getElementById('btn');
+const input = document.getElementById('input');
+const result = document.getElementById('result');
