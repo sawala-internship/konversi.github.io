2b8642b8e17c4d37836cb968ef6179a8e08378d5
Raditya-P-H <radityahidayat917@gmail.com>
2023-01-19 16:40:09 +0700

    mata-Uang

--- Konten/Mata Uang/mata-uang.js
+++ Konten/Mata Uang/mata-uang.js
@@ -1,41 +1,65 @@
-const select = document.querySelectorAll('.currency');
-const button = document.getElementById('btn');
-const number_box = document.getElementById('number_box');
-const answer_box = document.getElementById('answer_box');
+var input = document.getElementById('input');
+var result = document.getElementById('result');
+var inputType = document.getElementById('inputType');
+var resultType = document.getElementById('resultType');
+var inputTypeValue,resultTypeValue;
 
-fetch(`https://api.frankfurter.app/currencies`)
-  .then(resp => resp.json())
-  .then((data) => {
-    show(data);
-  })
+//now add listener
+input.addEventListener("keyup",myResult);
+inputType.addEventListener("change",myResult);
+resultType.addEventListener("change",myResult);
 
-  function show(data) {
-    const asArray = Object.entries(data);
+// taking initial value
+inputTypeValue = inputType.value;
+resultTypeValue = resultType.value;
+
+function myResult () {
+
+
+    inputTypeValue = inputType.value;
+    resultTypeValue = resultType.value;
+
+  
+         // Dollar to Rupiah
+    if(inputTypeValue === "Dollar" && resultTypeValue ==="Rupiah") {
+        result.value = Number(input.value) * 16000;
+          // Rupiah to Dollar
+    }else if(inputTypeValue === "Rupiah" && resultTypeValue ==="Dollar") {
+        result.value = Number(input.value) * 0.0000625;
+         // Dollar to Dollar
+    }else if(inputTypeValue === "Dollar" && resultTypeValue ==="Dollar") {
+       result.value = input.value;
+
+    }
+
+
+         // Euro to Rupiah
+    if(inputTypeValue === "Euro" && resultTypeValue === "Rupiah") {
+       result.value = Number(input.value) * 17000;
+       // Rupiah to Euro
     
-    for (let i = 0; i < asArray; i++) {
-       select[0].innerHTML += `<option value ="${asArray[i][0]}">"${asArray[i][1]}"</option>`
-       select[1].innerHTML += `<option value ="${asArray[i][0]}">"${asArray[i][1]}"</option>`
+        }else if(inputTypeValue === "Rupiah" && resultTypeValue === "Euro") {
+        result.value = Number(input.value) * 0.00005882353;
+         
+        // Rupiah to Rupiah
+    
+        }else if (inputTypeValue === "Rupiah" && resultTypeValue === "Rupiah") {
+       result.value = input.value;
     }
-  }
 
-  button.addEventListener('click', ()=>{
-    let currency_one = select = select[0].value;
-    let currency_two = select = select[1].value;
 
-    let value = number_box.value;
-    if (currency_one != currency_two) {
-        convert(currency_one, currency_two, value);
-    } else {
-        alert("Pilih Mata Uang!");
+        // Dollar to Euro
+    if(inputTypeValue === "Dollar" && resultTypeValue === "Euro") {
+        result.value = Number(input.value) * 0.92;
+
+          //Euro to Dollar
+        } else if (inputTypeValue === "Euro" && resultTypeValue === "Dollar") {
+        result.value = Number(input.value) * 1.08;
+
+        // Euro to Euro
+        }else if(inputTypeValue === "Euro" && resultTypeValue === "Euro") {
+        result.value = input.value;
     }
-  });
 
-  function convert(currency_one, currency_two, value) {
-    const host = "api.frankfurter.app";
-    fetch(`https://${host}/latest?amount=${value}&from=${currency_one}&to=${currency_two}`);
-    .then((val) => val.json())
-    .then((val) => {
-        answer_box.value = Object.values(val.rates)[0];
-    })
-   
-  } 
+
+}
